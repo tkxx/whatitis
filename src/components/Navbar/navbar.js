58c36420904b35a8a9dbe5153475eea8c968c42a
User <26051646+tkxx@users.noreply.github.com>
2020-02-14 22:34:00 -0500

    beginning navbar, landing page, footer

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <nav>
+        <ul>
+          <li>ABOUT</li>
+          <li>WORK</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
